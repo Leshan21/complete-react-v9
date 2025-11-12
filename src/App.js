9e420e6a8aac5cc2c94d0ae992e228dbf076b5ff
Leshan <leshanpasindu12@gmail.com>
2025-11-12 06:54:45 +0530

    basic react app create using scretch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Hello, React!")
+  );
+};
+
+
+const container = document.getElementById("root");
+const root = ReactDom.createRoot(container);
+root.render(React.createElement(App));
