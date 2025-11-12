7cfee18428d99bfc3a35ef463dd4ae6f29b369ed
Leshan <leshanpasindu12@gmail.com>
2025-11-12 07:07:06 +0530

    create components for reuse

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ const App = () => {
   );
 };
 
+const pizza = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "The pepperoni pizza"),
+    React.createElement("p", {}, "mozzarella, pepperoni, tomato sauce"),
+  ])
+}
 
 const container = document.getElementById("root");
-const root = ReactDom.createRoot(container);
-root.render(React.createElement(App));
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App));
+} else {
+  console.error('Root container not found: element with id "root"');
+}
