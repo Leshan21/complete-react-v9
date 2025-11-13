62eaa8d9d3106ae655d5720f26dfbe25c5051db8
Leshan <leshanpasindu12@gmail.com>
2025-11-13 18:53:35 +0530

    add props

--- PADRE_GINOS/src/App.jsx
+++ PADRE_GINOS/src/App.jsx
@@ -1,11 +1,13 @@
+import Pizza from "./Pizza.jsx";
 
-unction App() {
+function App() {
 
 
   return (
-    <>
-
-    </>
+   <div className="app">
+    <h1>Padro Gino's - Order Now </h1>
+    <Pizza name = "pepperoni"  description = "pep, chees, stuff"/>
+   </div>
   )
 }
 
