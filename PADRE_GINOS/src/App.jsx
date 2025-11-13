cced9b215fbef03738c5a6ad36a4ab23f10f4e24
Leshan <leshanpasindu12@gmail.com>
2025-11-13 19:27:01 +0530

    add images /cssusing api

--- PADRE_GINOS/src/App.jsx
+++ PADRE_GINOS/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
    <div className="app">
     <h1>Padro Gino's - Order Now </h1>
-    <Pizza name = "pepperoni"  description = "pep, chees, stuff"/>
-    <Pizza name = "veggie" description = "mushroom, onion, green pepper"/>
-    <Pizza name = "hawaiian" description = "ham, pineapple"/>
+    <Pizza name = "pepperoni"  description = "pep, chees, stuff" image={"/public/pizzas/pepperoni.webp"}/>
+    <Pizza name = "Americano" description = "mushroom, onion, green pepper" image={"/public/pizzas/big_meat.webp"}/>
+    <Pizza name = "hawaiian" description = "ham, pineapple" image={"/public/pizzas/hawaiian.webp"}/>
    </div>
   )
 }
